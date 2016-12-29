c64304300a803af0bbe97b39526ae9831a46fb2e
fbusabiaga <fbusabiaga@gmail.com>
2016-12-29 09:56:01 -0500

    main.cpp added

---
+++ src/header.h
@@ -0,0 +1,35 @@
+// Filename: header.h
+//
+// Copyright (c) 2010-2016, Florencio Balboa Usabiaga
+//
+// This file is part of Fluam
+//
+// Fluam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Fluam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Fluam. If not, see <http://www.gnu.org/licenses/>.
+
+
+#include <iostream>
+#include <string>
+#include <stdlib.h> //for pseudorandom numbers
+#include <time.h>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+#ifdef GLOBAL
+#define EXTERN_GLOBAL
+#else
+#define EXTERN_GLOBAL extern
+#endif
+
---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+// Filename: main.cpp
+//
+// Copyright (c) 2010-2016, Florencio Balboa Usabiaga
+//
+// This file is part of Fluam
+//
+// Fluam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Fluam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Fluam. If not, see <http://www.gnu.org/licenses/>.
+
+
+#include "header.h"
+
+int main(int argc, char* argv[]){
+
+
+  //Read a file with data for start the simulation
+  // if(!loadDataMain(argc,argv)) return 0;
+  
+  //Save GPU information
+  // if(!cudaDevice()) return 0;
+  
+  //Initialize random number generator
+  // if(!initializeRandomNumber()) return 0;
+
+  //Choose scheme
+
+  cout << "END" << endl;
+  return 1;
+}
