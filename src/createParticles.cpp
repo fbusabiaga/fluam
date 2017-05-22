0d704dde7bf8863dcbfe961fa93f8df857c52c4a
floren <fbusabiaga@gmail.com>
2017-05-22 12:38:00 -0400

    create simple square for 2D Stokes limit

--- src/createParticles.cpp
+++ src/createParticles.cpp
@@ -50,7 +50,7 @@ bool createParticles(){
     vxParticleI = new double [np];
     vyParticleI = new double [np];
     vzParticleI = new double [np];
-    if(quasiNeutrallyBuoyant2D or quasiNeutrallyBuoyant4pt2D or quasi2D){
+    if(quasiNeutrallyBuoyant2D or quasiNeutrallyBuoyant4pt2D or quasi2D or stokesLimit2D){
       simpleCubic(2);
     }
     else{
